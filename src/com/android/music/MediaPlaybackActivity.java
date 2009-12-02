38f6bda4a301f5c2a0b133eab84d55f7e90521cb
Jagan Devarajan <c_jagand@quicinc.com>
2009-12-02 10:50:10 -0800

    Music app: Fix for the issue of playback cursor stopping one sec before final duration.
    Music playback activity refreshes UI for every one second. If the music clip is
    19.8 seconds, the last refresh for the clip is at 19th second, and next refresh
    will show the status of the next music clip. Because of this UI shows as if the
    clip is being played for 19 seconds only.
    
    Fixed the issue by refreshing UI when the playback of a clip is completed.
    
    Conflicts:
    
    	src/com/android/music/MediaPlaybackActivity.java

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -497,6 +497,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
         f.addAction(MediaPlaybackService.PLAYSTATE_CHANGED);
         f.addAction(MediaPlaybackService.META_CHANGED);
         f.addAction(MediaPlaybackService.PLAYBACK_COMPLETE);
+        f.addAction(MediaPlaybackService.REFRESH_PROGRESSBAR);
         registerReceiver(mStatusListener, new IntentFilter(f));
         updateTrackInfo();
         long next = refreshNow();
@@ -1175,8 +1176,8 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
             long pos = mPosOverride < 0 ? mService.position() : mPosOverride;
             long remaining = 1000 - (pos % 1000);
             if ((pos >= 0) && (mDuration > 0)) {
-                mCurrentTime.setText(MusicUtils.makeTimeString(this, pos / 1000));
-                
+                mCurrentTime.setText(MusicUtils.makeTimeString(this, (pos + 500) / 1000));
+
                 if (mService.isPlaying()) {
                     mCurrentTime.setVisibility(View.VISIBLE);
                 } else {
@@ -1253,6 +1254,8 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                 }
             } else if (action.equals(MediaPlaybackService.PLAYSTATE_CHANGED)) {
                 setPauseButtonImage();
+            } else if (action.equals(MediaPlaybackService.REFRESH_PROGRESSBAR)) {
+                refreshNow();
             }
         }
     };
